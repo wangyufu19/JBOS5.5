3633076109456cea5bd85328b5b1abed4f68c14e
wangyufu19 <youfu.wang@gmail.com>
2020-01-02 09:13:26 +0800

    提交代码

---
+++ src/main/java/com/jbos/common/utils/UriCodeUtils.java
@@ -0,0 +1,13 @@
+package com.jbos.common.utils;
+
+/**
+ * UriCodeUtils
+ * @author youfu.wang
+ * @date 2019-09-28
+ */
+public class UriCodeUtils {
+    public static void encode(String str){
+
+    }
+
+}
